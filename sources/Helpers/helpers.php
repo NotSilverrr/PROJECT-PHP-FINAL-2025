58390bd5fa204b54eab4206da88c31ae2b64ad9b
Mathis Latimier <mathis.latimier@gmail.com>
2025-01-24 16:10:33 +0100

    Suppression des comentaire helper view

--- sources/Helpers/helpers.php
+++ sources/Helpers/helpers.php
@@ -2,18 +2,14 @@
 if (!function_exists('view')) {
     function view(string $viewPath, array $data = []): void
     {
-        // Convertir le chemin de type "articles.index" en "views/articles/index.php"
         $viewFile = __DIR__ . '/../views/' . str_replace('.', '/', $viewPath) . '.php';
 
-        // Vérifier si le fichier existe
         if (!file_exists($viewFile)) {
             throw new Exception("La vue [$viewPath] n'existe pas : $viewFile");
         }
 
-        // Extraire les variables du tableau pour qu'elles soient accessibles dans la vue
         extract($data);
 
-        // Inclure le fichier de la vue
         include $viewFile;
     }
 }
