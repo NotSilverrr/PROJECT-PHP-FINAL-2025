1dbd768e26d18739713f0b5a32cf468eb2385683
Julien <julien.le-moing@keepcool.fr>
2025-02-18 17:21:46 +0100

    oui

--- Project/App/Views/admin/user/user_form.php
+++ Project/App/Views/admin/user/user_form.php
@@ -1,45 +1,38 @@
-<div class="form-container-admin">
-    <div class="form-card-admin">
-        <h1 class="form-title"><?= isset($update) ? 'Update User' : 'Add User' ?></h1>
-        
-        <form method="POST" action="<?= isset($update) ? '/admin/user/update' : '/admin/user/add' ?>" class="form" enctype="multipart/form-data">
-            <?php if (isset($_SESSION['user_update'])): ?>
-                <input type="hidden" name="id" value="<?= $_SESSION['user_update']->id ?>" />
-            <?php endif; ?>        
-            <div class="input-group">
-                <label class="form-label">Email</label>
-                <input type="email" name="email" placeholder="Email" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->email : '' ?>" required />
-            </div>
-            <div class="input-group">
-                <label class="form-label">First Name</label>
-                <input type="text" name="first_name" placeholder="First Name" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->first_name : '' ?>" required />
-            </div>
-            <div class="input-group">
-                <label class="form-label">Last Name</label>
-                <input type="text" name="last_name" placeholder="Last Name" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->last_name : '' ?>" required />
-            </div>
-            <div class="input-group">
-                <label class="form-label"><?= isset($_SESSION['user_update']) ? 'Nouveau mot de passe (laisser vide pour ne pas modifier)' : 'Mot de passe' ?></label>
-                <input type="password" name="password" placeholder="" class="input-field" <?= !isset($_SESSION['user_update']) ? 'required' : '' ?> />
-            </div>
-            <div class="input-group">
-                <label class="checkbox">
-                    <input type="checkbox" name="is_admin" <?= isset($_SESSION['user_update']) && $_SESSION['user_update']->isadmin ? 'checked' : '' ?> />
-                    <span>Statut Admin</span>
-                </label>
-            </div>
-            <div class="input-group">
-                <label>Profile Picture</label>
-                <?php if(isset($_SESSION['user_update']) && $_SESSION['user_update']->profile_picture): ?>
-                    <img src="<?= $_SESSION['user_update']->profile_picture ?>" alt="Current profile picture" style="max-width: 100px; margin-bottom: 10px;">
-                <?php endif; ?>
-                <input type="file" name="profile_picture" accept="image/*" class="input-field" <?= !isset($_SESSION['user_update']) ? 'required' : '' ?> />
-            </div>
-            <button type="submit" class="form-button"><?= isset($_SESSION['user_update']) ? 'Update User' : 'Add User' ?></button>
-            <a href="/admin/user" class="create-account">Back to Users List</a>
-        </form>
+<h1 class="form-title"><?= isset($update) ? 'Update User' : 'Add User' ?></h1>
+
+<form method="POST" action="<?= isset($update) ? '/admin/user/update' : '/admin/user/add' ?>" class="form" enctype="multipart/form-data">
+    <?php if (isset($_SESSION['user_update'])): ?>
+        <input type="hidden" name="id" value="<?= $_SESSION['user_update']->id ?>" />
+    <?php endif; ?>        
+    <div>
+        <label class="form__label">Email</label>
+        <input type="email" name="email" placeholder="Email" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->email : '' ?>" required />
     </div>
-</div>
+    <div>
+        <label class="form__label">First Name</label>
+        <input type="text" name="first_name" placeholder="First Name" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->first_name : '' ?>" required />
+    </div>
+    <div>
+        <label class="form__label">Last Name</label>
+        <input type="text" name="last_name" placeholder="Last Name" class="input-field" value="<?= isset($_SESSION['user_update']) ? $_SESSION['user_update']->last_name : '' ?>" required />
+    </div>
+    <div>
+        <label class="form__label"><?= isset($_SESSION['user_update']) ? 'New password' : 'Password' ?></label>
+        <input type="password" name="password" placeholder="" class="input-field" <?= !isset($_SESSION['user_update']) ? 'required' : '' ?> />
+    </div>
+    <div>
+        <label class="checkbox">
+            <input type="checkbox" name="is_admin" <?= isset($_SESSION['user_update']) && $_SESSION['user_update']->isadmin ? 'checked' : '' ?> />
+            <span>Statut Admin</span>
+        </label>
+    </div>
+    <div>
+        <label>Profile Picture</label>
+        <input type="file" name="profile_picture" accept="image/*" class="input-field" <?= !isset($_SESSION['user_update']) ? 'required' : '' ?> />
+    </div>
+    <button type="submit" class="button button--primary"><?= isset($_SESSION['user_update']) ? 'Update User' : 'Add User' ?></button>
+    <a href="/admin/user" class="form__a">Back to Users List</a>
+</form>
 <?php
 unset($_SESSION['user_update']);
 ?>
