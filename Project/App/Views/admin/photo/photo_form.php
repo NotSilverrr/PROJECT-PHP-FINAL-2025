91231263a2f2e203974807e5c3c6e9fe2f1c0a34
NotSilverrr <tronsin7@gmail.com>
2025-02-18 17:14:13 +0100

    fix photo

--- Project/App/Views/admin/photo/photo_form.php
+++ Project/App/Views/admin/photo/photo_form.php
@@ -1,17 +1,16 @@
 
     <div class="">
-        <h1 class="form__title"><?= isset($photo) ? 'Update Photo' : 'Add Photo' ?></h1>
-        
+        <h1 class="form__title"><?= isset($update) ? 'Update Photo' : 'Add Photo' ?></h1>
         <form method="POST" action="<?= isset($update) ? '/admin/photo/update' : '/admin/photo/add' ?>" class="form form--full" enctype="multipart/form-data">
-        <?php if (isset($photo)): ?>
-            <input type="hidden" name="id" value="<?= $photo['id'] ?>" />
+        <?php if (isset($_SESSION['photo_update'])): ?>
+            <input type="hidden" name="id" value="<?= $_SESSION['photo_update']->id ?>" />
             <?php endif; ?>
             <div class="">
                 <label class="form__label">Photo</label>
-                <?php if(isset($photo) && $photo['file']): ?>
-                    <img src="<?= $photo['file'] ?>" alt="Current photo" style="max-width: 200px; margin-bottom: 10px;">
+                <?php if(isset($_SESSION['photo_update']) && $_SESSION['photo_update']->file): ?>
+                    <img src="<?= $_SESSION['photo_update']->file ?>" alt="Current photo" style="max-width: 200px; margin-bottom: 10px;">
                 <?php endif; ?>
-                <input type="file" name="photo" accept="image/*" class="" <?= !isset($photo) ? 'required' : '' ?> />
+                <input type="file" name="photo" accept="image/*" class="" <?= !isset($_SESSION['photo_update']) ? 'required' : '' ?> />
             </div>
             <div class="">
                 <label class="form__label">Group</label>
