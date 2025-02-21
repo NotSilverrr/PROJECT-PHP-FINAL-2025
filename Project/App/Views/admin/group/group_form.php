d8aeb2825a21da7d8d2d4ba64c4763a058cd90ec
NotSilverrr <tronsin7@gmail.com>
2025-02-21 13:33:48 +0100

    add admin form design guide

--- Project/App/Views/admin/group/group_form.php
+++ Project/App/Views/admin/group/group_form.php
@@ -59,15 +59,10 @@
                     <td>
                         <form method="POST" action="/admin/member/delete/<?= $_SESSION['group_update']->id ?>">
                             <input type="hidden" name="user_id" value="<?= $member->id ?>" />
-                            <button type="submit" class="many-to-many-table-delete button button--danger">
+                            <button type="submit" class="many-to-many-table-delete button button--danger button--sm ">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                                 <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
                             </svg>
-                            </button>
-                        </form>
-                        <form method="POST" action="/admin/member/toggle-readonly/<?= $_SESSION['group_update']->id ?>">
-                            <input type="hidden" name="user_id" value="<?= $member->id ?>" />
-                            <button type="submit" class="many-to-many-table-toggle" title="Toggle Read-only Status">
                             </button>
                         </form>
                     </td>
